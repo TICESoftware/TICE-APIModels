dde35a606da2922653127a3625c018eec5b6829c
Andreas Ganske <a.ganske@chaosspace.de>
2019-01-15 11:54:26 +0100

    Add Envelope and Payloads
    Use Envelope.Identifier and Envelope.Sender

---
+++ Sources/Models/Envelope.swift
@@ -0,0 +1,69 @@
+//
+//  Copyright © 2019 Anbion. All rights reserved.
+//  
+
+import Foundation
+
+public struct Envelope: Codable {
+    
+    public typealias Identifier = String
+    public typealias Sender = String
+    
+    public var id: Identifier
+    public var sender: Sender
+    public var payloadType: PayloadType
+    public var payload: Payload
+    
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case sender
+        case payloadType
+        case payload
+    }
+    
+    public enum PayloadType: String, Codable {
+        case encryptedMessageV1 = "encryptedMessage/v1"
+        case groupInvitationV1 = "groupInvitation/v1"
+        case envelopeBundleV1 = "envelopeBundle/v1"
+    }
+    
+    public init(id: Identifier, sender: Sender, payloadType: PayloadType, payload: Payload) {
+        self.id = id
+        self.sender = sender
+        self.payloadType = payloadType
+        self.payload = payload
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        id = try container.decode(Identifier.self, forKey: .id)
+        sender = try container.decode(Sender.self, forKey: .sender)
+        payloadType = try container.decode(PayloadType.self, forKey: .payloadType)
+        
+        switch payloadType {
+        case .encryptedMessageV1:
+            payload = try container.decode(EncryptedMessage.self, forKey: .payload)
+        case .groupInvitationV1:
+            payload = try container.decode(GroupInvitation.self, forKey: .payload)
+        case .envelopeBundleV1:
+            payload = try container.decode(EnvelopeBundle.self, forKey: .payload)
+        }
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(sender, forKey: .sender)
+        try container.encode(payloadType, forKey: .payloadType)
+        
+        switch payloadType {
+        case .encryptedMessageV1:
+            try container.encode(payload as! EncryptedMessage, forKey: .payload)
+        case .groupInvitationV1:
+            try container.encode(payload as! GroupInvitation, forKey: .payload)
+        case .envelopeBundleV1:
+            try container.encode(payload as! EnvelopeBundle, forKey: .payload)
+        }
+    }
+}
---
+++ Sources/Models/Payload.swift
@@ -0,0 +1,29 @@
+//
+//  Copyright © 2019 Anbion. All rights reserved.
+//  
+
+import Foundation
+
+public protocol Payload: Codable {}
+
+public struct EnvelopeBundle: Payload {
+    public let envelopes: [Envelope]
+}
+
+public struct EncryptedMessage: Payload {
+    public let encryptedMessage: String
+    public let encryptedMessageKey: String
+    
+    public init(encryptedMessage: String, encryptedMessageKey: String) {
+        self.encryptedMessage = encryptedMessage
+        self.encryptedMessageKey = encryptedMessageKey
+    }
+}
+
+public struct GroupInvitation: Payload {
+    public let groupId: String
+    
+    public init(groupId: String) {
+        self.groupId = groupId
+    }
+}
