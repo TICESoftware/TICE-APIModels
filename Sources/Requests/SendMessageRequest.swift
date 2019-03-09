51240ce1fbeb6509cb826a4b122367e78401d890
Fabio Tacke <f.tacke@anbion.de>
2019-03-09 12:43:26 +0100

    Add parameters to send message request

--- Sources/Requests/SendMessageRequest.swift
+++ Sources/Requests/SendMessageRequest.swift
@@ -6,17 +6,23 @@ public struct SendMessageRequest: Codable {
     public let timestamp: Date
     public let encryptedMessage: String
     public let encryptedMessageSignature: String
+    public let groupId: String?
+    public let oldGroupHash: String?
+    public let newGroupHash: String?
     public let serverSignedMembershipCertificate: Membership
     public let recipients: [Recipient]
     public let collapseId: String?
     public let messageTimeToLive: TimeInterval
 
-    public init(id: UUID, senderId: String, timestamp: Date, encryptedMessage: String, encryptedMessageSignature: String, serverSignedMembershipCertificate: Membership, recipients: [Recipient], collapseId: String?, messageTimeToLive: TimeInterval) {
+    public init(id: UUID, senderId: String, timestamp: Date, encryptedMessage: String, encryptedMessageSignature: String, groupId: String? = nil, oldGroupHash: String? = nil, newGroupHash: String? = nil, serverSignedMembershipCertificate: Membership, recipients: [Recipient], collapseId: String?, messageTimeToLive: TimeInterval) {
         self.id = id
         self.senderId = senderId
         self.timestamp = timestamp
         self.encryptedMessage = encryptedMessage
         self.encryptedMessageSignature = encryptedMessageSignature
+        self.groupId = groupId
+        self.oldGroupHash = oldGroupHash
+        self.newGroupHash = newGroupHash
         self.serverSignedMembershipCertificate = serverSignedMembershipCertificate
         self.recipients = recipients
         self.collapseId = collapseId
