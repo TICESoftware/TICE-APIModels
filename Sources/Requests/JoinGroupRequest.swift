94b7492da5e1983d6adc0bf25d284eafe25c383a
Andreas Ganske <a.ganske@chaosspace.de>
2019-03-21 10:48:58 +0100

    Fixup

--- Sources/Requests/JoinGroupRequest.swift
+++ Sources/Requests/JoinGroupRequest.swift
@@ -1,11 +1,11 @@
 public struct JoinGroupRequest: Codable {
     public let selfSignedMembershipCertificate: Membership
-    public let serverSignedOwnerCertificate: Membership?
+    public let serverSignedAdminCertificate: Membership?
     public let adminSignedMembershipCertificate: Membership?
 
-    public init(selfSignedMembershipCertificate: Membership, serverSignedOwnerCertificate: Membership? = nil, adminSignedMembershipCertificate: Membership? = nil) {
+    public init(selfSignedMembershipCertificate: Membership, serverSignedAdminCertificate: Membership? = nil, adminSignedMembershipCertificate: Membership? = nil) {
         self.selfSignedMembershipCertificate = selfSignedMembershipCertificate
-        self.serverSignedOwnerCertificate = serverSignedOwnerCertificate
+        self.serverSignedAdminCertificate = serverSignedAdminCertificate
         self.adminSignedMembershipCertificate = adminSignedMembershipCertificate
     }
 }
