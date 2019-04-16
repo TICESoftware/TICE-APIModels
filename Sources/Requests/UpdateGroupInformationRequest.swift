3d89ebd00349f39d9823930b70c2e32700149a69
Simon Kempendorf <simon@code28.de>
2019-04-16 15:38:11 +0200

    Remove group auth for group updates

--- Sources/Requests/UpdateGroupInformationRequest.swift
+++ Sources/Requests/UpdateGroupInformationRequest.swift
@@ -1,11 +1,8 @@
 public struct UpdateGroupInformationRequest: Codable {
-
-    public let serverSignedMembershipCertificate: Membership
     public let newSettings: String
     public let notificationRecipients: [NotificationRecipient]
 
-    public init(serverSignedMembershipCertificate: Membership, newSettings: String, notificationRecipients: [NotificationRecipient]) {
-        self.serverSignedMembershipCertificate = serverSignedMembershipCertificate
+    public init(newSettings: String, notificationRecipients: [NotificationRecipient]) {
         self.newSettings = newSettings
         self.notificationRecipients = notificationRecipients
     }
--- Sources/Requests/UpdateGroupInternalsRequest.swift
+++ Sources/Requests/UpdateGroupInternalsRequest.swift
@@ -1,11 +1,8 @@
 public struct UpdateGroupInternalsRequest: Codable {
-
-    public let serverSignedMembershipCertificate: Membership
     public let newInternalSettings: String
     public let notificationRecipients: [NotificationRecipient]
 
-    public init(serverSignedMembershipCertificate: Membership, newInternalSettings: String, notificationRecipients: [NotificationRecipient]) {
-        self.serverSignedMembershipCertificate = serverSignedMembershipCertificate
+    public init(newInternalSettings: String, notificationRecipients: [NotificationRecipient]) {
         self.newInternalSettings = newInternalSettings
         self.notificationRecipients = notificationRecipients
     }
