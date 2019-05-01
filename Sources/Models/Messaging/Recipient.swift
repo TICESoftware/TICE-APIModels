bd220e61c1be7693afb8365e10a444642593aea2
Fabio Tacke <f.tacke@anbion.de>
2019-05-01 11:40:01 +0200

    Remove identity key from recipient

--- Sources/Models/Messaging/Recipient.swift
+++ Sources/Models/Messaging/Recipient.swift
@@ -6,13 +6,11 @@ import Foundation
 
 public struct Recipient: Codable, Hashable {
     public let userId: UserId
-    public let identityKey: String
     public let serverSignedMembershipCertificate: Membership
     public let encryptedMessageKey: String
 
-    public init(userId: UserId, identityKey: String, serverSignedMembershipCertificate: Membership, encryptedMessageKey: String) {
+    public init(userId: UserId, serverSignedMembershipCertificate: Membership, encryptedMessageKey: String) {
         self.userId = userId
-        self.identityKey = identityKey
         self.serverSignedMembershipCertificate = serverSignedMembershipCertificate
         self.encryptedMessageKey = encryptedMessageKey
     }
