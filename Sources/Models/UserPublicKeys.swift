7ade6fad199cf0f2ed229a24ef5d003dd3d93870
Fabio Tacke <f.tacke@anbion.de>
2019-05-01 11:23:13 +0200

    Refactor public keys

--- Sources/Models/UserPublicKeys.swift
+++ Sources/Models/UserPublicKeys.swift
@@ -5,15 +5,9 @@
 import Foundation
 
 public struct UserPublicKeys: Codable, Hashable {
-    public let identityKey: String
-    public let ephemeralKey: String
-    public let signedPreKey: String
-    public let preKeys: [String]
+    public let certificateKey: String
 
-    public init(identityKey: String, ephemeralKey: String, signedPreKey: String, preKeys: [String]) {
-        self.identityKey = identityKey
-        self.ephemeralKey = ephemeralKey
-        self.signedPreKey = signedPreKey
-        self.preKeys = preKeys
+    public init(certificateKey: String) {
+        self.certificateKey = certificateKey
     }
 }
