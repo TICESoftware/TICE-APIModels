876e1141448ff6bf7a592941522b894919195647
Simon Kempendorf <simon@code28.de>
2019-05-15 13:27:55 +0200

    Use correct typealias for tokenKey

--- Sources/Requests/AddGroupMemberRequest.swift
+++ Sources/Requests/AddGroupMemberRequest.swift
@@ -3,10 +3,10 @@ import Foundation
 public struct AddGroupMemberRequest: Codable {
     public let encryptedMembership: Ciphertext
     public let serverSignedMembershipCertificate: Certificate
-    public let newTokenKey: Data
+    public let newTokenKey: SecretKey
     public let notificationRecipients: [NotificationRecipient]
 
-    public init(encryptedMembership: Ciphertext, serverSignedMembershipCertificate: Certificate, newTokenKey: Data, notificationRecipients: [NotificationRecipient]) {
+    public init(encryptedMembership: Ciphertext, serverSignedMembershipCertificate: Certificate, newTokenKey: SecretKey, notificationRecipients: [NotificationRecipient]) {
         self.encryptedMembership = encryptedMembership
         self.serverSignedMembershipCertificate = serverSignedMembershipCertificate
         self.newTokenKey = newTokenKey
