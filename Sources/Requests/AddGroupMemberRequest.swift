a363d970fca25fca7405c6c7b55acd587396b856
Simon Kempendorf <simon@code28.de>
2019-05-28 17:49:24 +0200

    Set tokenKey type to string

--- Sources/Requests/AddGroupMemberRequest.swift
+++ Sources/Requests/AddGroupMemberRequest.swift
@@ -3,10 +3,10 @@ import Foundation
 public struct AddGroupMemberRequest: Codable {
     public let encryptedMembership: Ciphertext
     public let userId: UserId
-    public let newTokenKey: SecretKey
+    public let newTokenKey: String
     public let notificationRecipients: [NotificationRecipient]
 
-    public init(encryptedMembership: Ciphertext, userId: UserId, newTokenKey: SecretKey, notificationRecipients: [NotificationRecipient]) {
+    public init(encryptedMembership: Ciphertext, userId: UserId, newTokenKey: String, notificationRecipients: [NotificationRecipient]) {
         self.encryptedMembership = encryptedMembership
         self.userId = userId
         self.newTokenKey = newTokenKey
--- Sources/Requests/DeleteGroupMemberRequest.swift
+++ Sources/Requests/DeleteGroupMemberRequest.swift
@@ -1,13 +1,11 @@
 public struct DeleteGroupMemberRequest: Codable {
     public let userId: UserId
     public let serverSignedMembershipCertificate: Certificate
-    public let tokenKey: SecretKey
     public let notificationRecipients: [NotificationRecipient]
 
-    public init(userId: UserId, serverSignedMembershipCertificate: Certificate, tokenKey: SecretKey, notificationRecipients: [NotificationRecipient]) {
+    public init(userId: UserId, serverSignedMembershipCertificate: Certificate, notificationRecipients: [NotificationRecipient]) {
         self.userId = userId
         self.serverSignedMembershipCertificate = serverSignedMembershipCertificate
-        self.tokenKey = tokenKey
         self.notificationRecipients = notificationRecipients
     }
 }
