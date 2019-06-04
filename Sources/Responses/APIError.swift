3b8321da7eb54928a99f1c2eb66f4a312c663182
Fabio Tacke <f.tacke@anbion.de>
2019-06-04 10:42:53 +0200

    Add certificates to envelope and add error payload

--- Sources/Responses/APIError.swift
+++ Sources/Responses/APIError.swift
@@ -2,6 +2,7 @@ public struct APIError: Error, Equatable {
 
     public let type: ErrorType
     public let description: String
+    public let errorPayload: ErrorPayload?
 
     public enum ErrorType: String, Codable {
         case unknown
@@ -18,9 +19,10 @@ public struct APIError: Error, Equatable {
         case notModified
     }
 
-    public init(type: ErrorType, description: String = "") {
+    public init(type: ErrorType, description: String = "", payload: ErrorPayload? = nil) {
         self.type = type
         self.description = description
+        self.errorPayload = payload
     }
 }
 
@@ -38,5 +40,28 @@ extension APIError: Codable {
         let description = try values.decode(String.self, forKey: .description)
 
         self.init(type: ErrorType(rawValue: errorTypeString) ?? .unknown, description: description)
+    }
+}
+
+public struct ErrorPayload: Codable, Equatable {
+    public let conflicts: [Conflict]
+
+    public struct Conflict: Codable, Equatable {
+        public let userId: UserId
+        public let reason: Reason
+
+        public enum Reason: String, Codable {
+            case newIdentityKey
+            case membershipMismatch
+        }
+
+        public init(userId: UserId, reason: Reason) {
+            self.userId = userId
+            self.reason = reason
+        }
+    }
+
+    public init(conflicts: [Conflict]) {
+        self.conflicts = conflicts
     }
 }
