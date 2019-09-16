b8f9ba4a472c27ecbf0c9969faed83f01e0c65fa
Fabio Tacke <f.tacke@anbion.de>
2019-09-16 20:19:26 +0200

    Add server signed membership parameter to delete group member request

--- Sources/Requests/DeleteGroupMemberRequest.swift
+++ Sources/Requests/DeleteGroupMemberRequest.swift
@@ -1,9 +1,11 @@
 public struct DeleteGroupMemberRequest: Codable {
     public let userId: UserId
+    public let serverSignedMembershipCertificate: Certificate
     public let notificationRecipients: [NotificationRecipient]
 
-    public init(userId: UserId, notificationRecipients: [NotificationRecipient]) {
+    public init(userId: UserId, serverSignedMembershipCertificate: Certificate, notificationRecipients: [NotificationRecipient]) {
         self.userId = userId
+        self.serverSignedMembershipCertificate = serverSignedMembershipCertificate
         self.notificationRecipients = notificationRecipients
     }
 }
