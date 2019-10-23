bb8b84aa66267b6a54aa97712550fac55dbd7011
Fabio Tacke <f.tacke@anbion.de>
2019-10-23 12:03:41 +0200

    Add platform property to verify request

--- Sources/Requests/VerifyRequest.swift
+++ Sources/Requests/VerifyRequest.swift
@@ -1,9 +1,11 @@
 public typealias DeviceId = String
 
 public struct VerifyRequest: Codable {
+    public let platform: Platform
     public let deviceId: DeviceId
     
-    public init(deviceId: DeviceId) {
+    public init(platform: Platform, deviceId: DeviceId) {
+        self.platform = platform
         self.deviceId = deviceId
     }
 }
