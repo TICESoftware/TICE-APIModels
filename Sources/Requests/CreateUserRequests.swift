0ba0019c439b80893071251be4efc0ec83dd4f1f
Simon Kempendorf <simon@code28.de>
2021-08-05 12:51:23 +0200

    Add create user request with captcha

--- Sources/Requests/CreateUserRequests.swift
+++ Sources/Requests/CreateUserRequests.swift
@@ -1,6 +1,6 @@
 public typealias PhoneNumber = String
 
-public struct CreateUserRequest: Codable {
+public struct CreateUserPushRequest: Codable {
     public let publicKeys: UserPublicKeys
     public let platform: Platform
     public let deviceId: String
@@ -11,6 +11,20 @@ public struct CreateUserRequest: Codable {
         self.publicKeys = publicKeys
         self.platform = platform
         self.deviceId = deviceId
+        self.verificationCode = verificationCode
+        self.publicName = publicName
+    }
+}
+
+public struct CreateUserCaptchaRequest: Codable {
+    public let publicKeys: UserPublicKeys
+    public let platform: Platform
+    public let verificationCode: String
+    public let publicName: String?
+
+    public init(publicKeys: UserPublicKeys, platform: Platform, verificationCode: String, publicName: String?) {
+        self.publicKeys = publicKeys
+        self.platform = platform
         self.verificationCode = verificationCode
         self.publicName = publicName
     }
